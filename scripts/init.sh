c2b9034814996386465fc4078dcbc661f3cf6bdb
Ryan <rdmiers@gmail.com>
2026-07-03 11:57:03 -0400

    Remove commented-out Korean debug line

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,7 +3,6 @@
 FEXBash "/home/steam/Steam/steamcmd.sh +force_install_dir /home/steam/pzserver +login anonymous +app_update 380870 validate +quit"
 screen -d -m -S zomboid /bin/bash -c ' \
   while true; do \
-    # sed -i "s/UseZGC/UseG1GC/g" /home/steam/pzserver/ProjectZomboid64.json; \
     FEXBash "/home/steam/pzserver/start-server.sh -servername ${SERVERNAME}"; \
     echo "The server will restart in 10 seconds. If you want to stop the server, press Ctrl+C."; \
     for i in 10 9 8 7 6 5 4 3 2 1; do echo "$i..."; sleep 1; done \
